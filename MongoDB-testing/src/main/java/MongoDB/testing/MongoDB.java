6869cabda9d1cd6af2b006b11df22517fa2a2761
Ashar Ali <ashar.alia1999@gmail.com>
2021-04-20 22:14:02 +0500

    Done with Database Connectivity and test trial

--- MongoDB-testing/src/main/java/MongoDB/testing/MongoDB.java
+++ MongoDB-testing/src/main/java/MongoDB/testing/MongoDB.java
@@ -1,9 +1,33 @@
 package MongoDB.testing;
 
-//you need a client to run on. we do this as mongoclient
+import java.net.UnknownHostException;
+
+import com.mongodb.*;
+//you need a client to run on. we do this as Mongo client
 
 public class MongoDB {
-	public static void main(String arg[]) {
+	public static MongoClient mongoClient;
+	public static DB database;
+	public static DBCollection test;
+	public static void main(String arg[])throws UnknownHostException {
+		mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost:27017"));
+		database = mongoClient.getDB("MongoDB-testing"); // in parameter, we are adding database name 
+		// the reason why we have getDB is because getDB first check if there is existing database, if not then it create new one
+		 test = database.getCollection("test");
+//		 DBObject DatabaseObj = new BasicDBObject("employee","ashar").append("items",new int[]).append("table","table5"); // can be done by this too but we have better way 
+		 TestObj testObj = new TestObj();
+		 testObj.setMemberID("Ashar");
+		 testObj.setTimer(1);
+		 testObj.setXp(1234);
+		 //Now insert our items in database;
+		 test.insert(convert(testObj));
+		 DBObject query = new BasicDBObject("XP",1234); // means that we want specific obj of data whose XP is 1234
+		 DBCursor cursor= test.find(query); // we need a cursor to take that database and find query. if there is more than one satisfying the query it will return
+		 // if we want just first one then we use cursor.one()
+		 System.out.println(cursor);
+	}
+	public static DBObject convert(TestObj testObj) {
+		return new BasicDBObject("XP",testObj.getXp()).append("Timer",testObj.getTimer()).append("Member ID", testObj.getMemberID());
 		
 	}
 }
---
+++ MongoDB-testing/src/main/java/MongoDB/testing/TestObj.java
@@ -0,0 +1,35 @@
+package MongoDB.testing;
+
+public class TestObj {
+	private String memberID = "";
+	private int timer = 0;
+	private int xp = 0 ;
+	
+	public TestObj() {
+		
+	}
+
+	public String getMemberID() {
+		return memberID;
+	}
+
+	public void setMemberID(String memberID) {
+		this.memberID = memberID;
+	}
+
+	public int getTimer() {
+		return timer;
+	}
+
+	public void setTimer(int timer) {
+		this.timer = timer;
+	}
+
+	public int getXp() {
+		return xp;
+	}
+
+	public void setXp(int xp) {
+		this.xp = xp;
+	}
+}
