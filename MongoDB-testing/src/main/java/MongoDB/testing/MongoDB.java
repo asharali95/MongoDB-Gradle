6d3b107a40ba613498a691fcac46ce8634b8dcce
Ashar Ali <ashar.alia1999@gmail.com>
2021-04-20 23:59:00 +0500

    document field modification testing

--- MongoDB-testing/src/main/java/MongoDB/testing/MongoDB.java
+++ MongoDB-testing/src/main/java/MongoDB/testing/MongoDB.java
@@ -15,16 +15,37 @@ public class MongoDB {
 		// the reason why we have getDB is because getDB first check if there is existing database, if not then it create new one
 		 test = database.getCollection("test");
 //		 DBObject DatabaseObj = new BasicDBObject("employee","ashar").append("items",new int[]).append("table","table5"); // can be done by this too but we have better way 
-		 TestObj testObj = new TestObj();
-		 testObj.setMemberID("Ashar");
-		 testObj.setTimer(1);
-		 testObj.setXp(1234);
+
+//		 TestObj testObj = new TestObj();
+//		 testObj.setMemberID("Ashar");
+//		 testObj.setTimer(1);
+//		 testObj.setXp(1234);
+		 
 		 //Now insert our items in database;
-		 test.insert(convert(testObj));
+		 
+//		 test.insert(convert(testObj));
+		 
 		 DBObject query = new BasicDBObject("XP",1234); // means that we want specific obj of data whose XP is 1234
 		 DBCursor cursor= test.find(query); // we need a cursor to take that database and find query. if there is more than one satisfying the query it will return
 		 // if we want just first one then we use cursor.one()
-		 System.out.println(cursor);
+		 
+//		 while(cursor.hasNext()) {
+//			 DBObject next = cursor.next();
+//			 System.out.println(next);
+//		 }
+		 
+		 TestObj newMod = new TestObj();
+		 newMod.setXp(5678);
+		 newMod.setMemberID("Ashar");
+		 newMod.setTimer(1);
+		 
+//		 test.findAndModify(query,convert(newMod)); // for modification in test collection
+		 cursor = test.find(query);
+		 
+		 while(cursor.hasNext()) {
+			 DBObject next = cursor.next();
+			 System.out.println(next);
+		 }
 	}
 	public static DBObject convert(TestObj testObj) {
 		return new BasicDBObject("XP",testObj.getXp()).append("Timer",testObj.getTimer()).append("Member ID", testObj.getMemberID());
