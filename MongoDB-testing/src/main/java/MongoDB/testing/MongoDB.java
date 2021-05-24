2ee3be47e358ed42ea465b02a35179d0bfb4b6be
Ashar Ali <ashar.alia1999@gmail.com>
2021-05-24 12:25:07 +0500

    demo commit for scd lab

--- MongoDB-testing/src/main/java/MongoDB/testing/MongoDB.java
+++ MongoDB-testing/src/main/java/MongoDB/testing/MongoDB.java
@@ -1,7 +1,6 @@
 package MongoDB.testing;
 
 import java.net.UnknownHostException;
-
 import com.mongodb.*;
 //you need a client to run on. we do this as Mongo client
 
@@ -16,32 +15,32 @@ public class MongoDB {
 		 test = database.getCollection("test");
 //		 DBObject DatabaseObj = new BasicDBObject("employee","ashar").append("items",new int[]).append("table","table5"); // can be done by this too but we have better way 
 
-//		 TestObj testObj = new TestObj();
-//		 testObj.setMemberID("Ashar");
-//		 testObj.setTimer(1);
-//		 testObj.setXp(1234);
+		 TestObj testObj = new TestObj();
+		 testObj.setMemberID("Ashar");
+		 testObj.setTimer(1);
+		 testObj.setXp(1234);
 		 
 		 //Now insert our items in database;
 		 
-//		 test.insert(convert(testObj));
+		 test.insert(convert(testObj));
 		 
-//		 DBObject query = new BasicDBObject("XP",1234); // means that we want specific obj of data whose XP is 1234
+		 DBObject query = new BasicDBObject("XP",1234); // means that we want specific obj of data whose XP is 1234
 		 
-//		 DBCursor cursor= test.find(query); // we need a cursor to take that database and find query. if there is more than one satisfying the query it will return
+		 DBCursor cursor= test.find(query); // we need a cursor to take that database and find query. if there is more than one satisfying the query it will return
 		 // if we want just first one then we use cursor.one()
 		 
-//		 while(cursor.hasNext()) {
-//			 DBObject next = cursor.next();
-//			 System.out.println(next);
-//		 }
+		 while(cursor.hasNext()) {
+			 DBObject next = cursor.next();
+			 System.out.println(next);
+		 }
 		 
-//		 TestObj newMod = new TestObj();
-//		 newMod.setXp(9101);
-//		 newMod.setMemberID("Zeeshan");
-//		 newMod.setTimer(3);
-//		 test.insert(convert(newMod));
-		 DBObject query = new BasicDBObject("XP",1234);
-		 DBCursor cursor= test.find(query);
+		 TestObj newMod = new TestObj();
+		 newMod.setXp(9101);
+		 newMod.setMemberID("Zeeshan");
+		 newMod.setTimer(3);
+		 test.insert(convert(newMod));
+//		 DBObject query = new BasicDBObject("XP",1234);
+//		 DBCursor cursor= test.find(query);
 //		 test.findAndModify(query,convert(newMod)); // for modification in test collection
 //		 cursor = test.find(query);
 		 
